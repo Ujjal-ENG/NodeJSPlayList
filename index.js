c4df3997b816a4a10033b6355e6318360dcf051d
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-07 23:38:21 +0600

    Send the data using form url encoded

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.urlencoded());
+
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
 app.post('/', (req, res) => {
+  console.log(req.body);
   res.send('This is the post route');
 });
 
