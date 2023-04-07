2797579008630a7501c241609db5b8369125b1e1
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-07 20:36:59 +0600

    successfully run my server

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 // dependencies
 const http = require('http');
+
 // app object - module scaffolding
 const app = {};
+
 // configuration
-app.config = {};
-http.createServer();
+app.config = {
+  port: 3000,
+};
+
+// create server
+app.createServer = () => {
+  const server = http.createServer(app.handleReqRes);
+  server.listen(app.config.port, () => {
+    console.log(`listening to port localhost:${app.config.port}`);
+  });
+};
+
+// handle Request and Response
+app.handleReqRes = (req, res) => {
+  res.end('Hello World');
+};
+// start the server
+app.createServer();
