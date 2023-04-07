591e2b4f3dc4681e7ea4f5bf66fdf03dad8ec96c
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-08 00:31:15 +0600

    Set the global path which is shown when path is not matched in the router

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ router.post('/', (req, res) => {
   console.log(req.body);
   res.send('This is the post route');
 });
-
+router.all('/*', (req, res) => {
+  res.send('This is the kopa Kopi path which is not valid path!!');
+});
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
 });
