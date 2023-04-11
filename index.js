0b007c6c271df75f4212c86dc88ba023bcf4c741
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-11 20:22:26 +0600

    make the very simple ui

--- index.js
+++ index.js
@@ -3,40 +3,14 @@
 import express from 'express';
 
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded());
-
-// eslint-disable-next-line arrow-body-style
-const loggerWrapper = (options) => {
-  // eslint-disable-next-line func-names
-  return function (req, res, next) {
-    if (options.log) {
-      console.log(
-        `${new Date(Date.now()).toLocaleString()} - ${req.method} - ${req.originalUrl} - ${
-          req.protocol
-        } - ${req.ip}`
-      );
-      next();
-    } else {
-      throw new Error('Mama application kop khaiya gese');
-    }
-  };
-};
-app.use(loggerWrapper({ log: false }));
-
-app.get('/about', (req, res) => {
-  res.send('About');
-  res.end();
-});
-app.all('/*', (req, res) => {
-  res.send('This is the kopa Kopi path which is not valid path!!');
+app.get('/', (req, res) => {
+  res.status(200).json({ msg: 'hei this the home page..' });
 });
 
-const errorMiddleWare = (err, req, res, next) => {
-  res.send('There was serbver site error');
-  next();
-};
-app.use(errorMiddleWare);
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+app.post('/', (req, res) => {
+  res.send('You can post to the endpoint...');
+});
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port localhost:${port}`);
 });
