44e72b98f30edfd765e01b2e0f893aa6c011e936
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-12 00:52:45 +0600

    send a chunk of Data from the server to the client site

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 /* eslint-disable comma-dangle */
 /* eslint-disable import/no-extraneous-dependencies */
-import express from 'express';
+const express = require('express');
+const Data = require('./public/dev-data/data.json');
 
 const app = express();
 app.get('/', (req, res) => {
   res.status(200).json({ msg: 'hei this the home page..' });
 });
+app.use(express.json());
 
 app.get('/api/v1/tours', (req, res) => {
-  res.status(200).json('THis is the fisrt tours api');
+  res.status(200).json({
+    status: 'success',
+    results: Data.length,
+    Data,
+  });
 });
+
+
+
 app.post('/', (req, res) => {
   res.send('You can post to the endpoint...');
 });
