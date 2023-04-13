82ee694d2891cff4aa443457fb803c0888afe418
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-13 21:17:26 +0600

    successfully run the server and go to root node

--- learn-nodejs/index.js
+++ learn-nodejs/index.js
@@ -1,5 +1,15 @@
-// dependencies
-// app object - module scaffolding
-const app = {};
-// configuration
-app.config = {};
+/* eslint-disable no-console */
+/* eslint-disable indent */
+/* eslint-disable import/no-extraneous-dependencies */
+import express from 'express';
+
+const app = express();
+app.get('/', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        message: 'Hei this the root page',
+    });
+});
+app.listen(3000, () => {
+    console.log(`The server is run at port: ${3000}`);
+});
